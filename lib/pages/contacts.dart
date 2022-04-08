25d1107052e3a899cf0a7de6f7275c935eb0d495
Altay Taneri <taneri862@gmail.com>
2022-04-08 12:33:37 +0300

    school address added to contracts screen

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -60,6 +60,33 @@ class _ContactsState extends State<Contacts> {
                         Image.asset('assets/smyrna.png', width: 100.0, height: 100.0),
                         const SizedBox(width: 50.0),
                         const Expanded(child: Text('CONTENT WILL BE HERE SOON!!', style: TextStyle(fontStyle: FontStyle.italic,fontWeight: FontWeight.bold, fontSize: 20.0))),
+                        const SizedBox(height: 50.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Divider(height: 50.0, thickness: 5, color: Colors.black),
+                            Image.asset('assets/University.png', width: 200.0, height: 100.0),
+                            const Divider(height: 50.0,thickness: 5, color: Colors.black)
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                const Text('Sakarya Caddesi No:156', style: TextStyle(fontWeight: FontWeight.bold)),
+                                const Text('35330 Balçova - İzmir/Türkiye', style: TextStyle(fontWeight: FontWeight.bold)),
+                                Row(
+                                  children: const <Widget>[
+                                    Icon(Icons.copyright),
+                                    Text('Smyrna Courts of Justice', style: TextStyle(fontWeight: FontWeight.bold))
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 100.0)
                       ],
                     ),
                   ),
